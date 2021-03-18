06bfdbf64680bfdd78c9fec1100f4f63fa004eee
NatyaBA <73196083+NatyaBA@users.noreply.github.com>
2021-03-18 23:24:17 +0300

    Add files via upload

--- My/message.js
+++ My/message.js
@@ -1,21 +1,44 @@
-function testCont(){
-    if( form1.FIO.value == "" ){
-        form1.FIO.select();
-        alert("Введите Имя");
-        return false;
-    }
-
-    if( form1.EMail.value == "" ){
-        form1.EMail.select();
-        alert("Введите E-mail");
-        return false;
-    }
-
-    if( form1.Title.value == "" ){
-        form1.Title.select();
-        alert("Введите сообщение");
-        return false;
-    }
-
-    return true;
+document.getElementById("clear").onclick = function() {
+    document.getElementById("text").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("textarea").value = "";
+    document.getElementById("website").value = "";
 }
+
+var modal = document.getElementById("my_modal");
+var btn = document.getElementById("btn_modal_window");
+var span = document.getElementsByClassName("close_modal_window")[0];
+
+
+ function validate(form_id, email) {
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    var address = document.forms[form_id].elements[email].value;
+
+    if (reg.test(address) == false) {
+         alert('Enter the correct e-mail');
+         return false;
+    }
+    else {
+        btn.onclick = function () {
+            modal.style.display = "block";
+        }
+
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }   
+    }
+}
+btn.onclick = validate;
+
+
+
+      
+  
+  
+
